53f030aca8abac6843a0e1a6d206280dcf7f8121
Tien <manhtien790@gmail.com>
2023-07-07 00:33:50 +0700

    lý do thêm code

--- src/modules/BaiTapThucHanhLayOut/productItems.jsx
+++ src/modules/BaiTapThucHanhLayOut/productItems.jsx
@@ -6,7 +6,7 @@ export default class ProductItems extends Component {
             <section className='py-5'>
                 <div className="container px-lg-5">
                     <div className="row gx-lg-5">
-                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17rem' }}>
+                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17.5rem' }}>
                             <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
@@ -14,7 +14,7 @@ export default class ProductItems extends Component {
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
-                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17rem' }}>
+                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17.5rem' }}>
                             <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
@@ -22,7 +22,7 @@ export default class ProductItems extends Component {
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
-                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17rem' }}>
+                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17.5rem' }}>
                             <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
@@ -30,7 +30,7 @@ export default class ProductItems extends Component {
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
-                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17rem' }}>
+                        <div className="card bg-light m-3 col-xxl-3 col-lg-4" style={{ width: '17.5rem' }}>
                             <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
